a1eb4acb3e9a8fdbb5d0c6789aa5d7457661afc8
Jaffar Hussein <jaffhussein@icloud.com>
2023-02-11 20:52:55 +0100

    Adds a drop down meni to the Navigation bar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,7 @@
 import logo from '../assets/m.svg'
 import logo_dark from '../assets/logo.svg'
+import * as DropdownMenu from '@radix-ui/react-dropdown-menu';
+import { motion,AnimatePresence } from "framer-motion"
 function Navbar(){
     return (
         <>
@@ -20,12 +22,37 @@ function Navbar(){
                     <div className="hidden md:flex float-right space-x-12  ">
                         <a href="#" className="text-accentTextContrast dark:text-accentTextContrast-dark font-medium hover:text-accentText dark:hover:text-accentText-dark">Home</a>
                         <a href="#" className="text-accentTextContrast dark:text-accentTextContrast-dark font-medium hover:text-accentText dark:hover:text-accentText-dark">About us</a>
-                        <a href="#" className="text-accentTextContrast dark:text-accentTextContrast-dark font-medium hover:text-accentText dark:hover:text-accentText-dark">Courses</a>
+                        {/* Dropdown Menu for courses */}
+                        <DropdownMenu.Root>
+                            <DropdownMenu.Trigger asChild>
+                                <a href="#" className="text-accentTextContrast focus-visible:outline-none dark:text-accentTextContrast-dark font-medium hover:text-accentText dark:hover:text-accentText-dark">
+                                    <div className='flex items-center'> 
+                                    Courses
+                                <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M3.13523 6.15803C3.3241 5.95657 3.64052 5.94637 3.84197 6.13523L7.5 9.56464L11.158 6.13523C11.3595 5.94637 11.6759 5.95657 11.8648 6.15803C12.0536 6.35949 12.0434 6.67591 11.842 6.86477L7.84197 10.6148C7.64964 10.7951 7.35036 10.7951 7.15803 10.6148L3.15803 6.86477C2.95657 6.67591 2.94637 6.35949 3.13523 6.15803Z" fill="currentColor" fillRule="evenodd" clipRule="evenodd" className=''></path></svg>
+                                    </div>
+                                  
+                                    </a>
+                            </DropdownMenu.Trigger>
+                            <DropdownMenu.Portal className=''>
+                            <DropdownMenu.Content  sideOffset={5} className="bg-accentBase dark:bg-accentBase-dark w-48 rounded-lg px-1.5 py-1 md:w-56 transition-all text-accentText dark:text-accentTextContrast-dark p-5 rounded-lg shadow-accentSolid dark:shadow-accentSolid-dark shadow-sm">
+                            <DropdownMenu.Item className="font-light hover:bg-accentBgHover hover:dark:bg-accentBgHover-dark outline-none rounded-md p-1">Initiation</DropdownMenu.Item>
+                            <DropdownMenu.Item className="font-light hover:bg-accentBgHover hover:dark:bg-accentBgHover-dark outline-none rounded-md p-1">Tafsir</DropdownMenu.Item>
+                            <DropdownMenu.Item className="font-light hover:bg-accentBgHover hover:dark:bg-accentBgHover-dark outline-none rounded-md p-1">Understandin</DropdownMenu.Item>
+                            <DropdownMenu.Item className="font-light hover:bg-accentBgHover hover:dark:bg-accentBgHover-dark outline-none rounded-md p-1">Application</DropdownMenu.Item>
+                            <DropdownMenu.Item className="font-light hover:bg-accentBgHover hover:dark:bg-accentBgHover-dark outline-none rounded-md p-1">Memorization</DropdownMenu.Item>
+
+                            </DropdownMenu.Content>
+                            </DropdownMenu.Portal>
+                        </DropdownMenu.Root>
                         <a href="#" className="text-accentTextContrast dark:text-accentTextContrast-dark font-medium hover:text-accentText dark:hover:text-accentText-dark">Contact Us</a>
                         <div className="flex">
                         <input type="checkbox" id="dark-mode-toggle" className="hidden"  onChange={()=>document.documentElement.classList.toggle('dark')}/>
                         <label htmlFor="dark-mode-toggle" className="w-full h-full transition-transform bg-gray-800 dark:bg-white rounded-full p-1 flex justify-between cursor-pointer">
                             {/* <span className="inline dark:hidden">🌞</span> */}
+                            <AnimatePresence>
+                            <motion.div layout
+                            exit={{ opacity: 0, transition: { duration: 0.2 } }}
+                            >
                             <span className="hidden dark:inline ">
                             <svg  width="15" height="15" viewBox="0 0 15 15"  xmlns="http://www.w3.org/2000/svg"><path d="M7.5 0C7.77614 0 8 0.223858 8 0.5V2.5C8 2.77614 7.77614 3 7.5 3C7.22386 3 7 2.77614 7 2.5V0.5C7 0.223858 7.22386 0 7.5 0ZM2.1967 2.1967C2.39196 2.00144 2.70854 2.00144 2.90381 2.1967L4.31802 3.61091C4.51328 3.80617 4.51328 4.12276 4.31802 4.31802C4.12276 4.51328 3.80617 4.51328 3.61091 4.31802L2.1967 2.90381C2.00144 2.70854 2.00144 2.39196 2.1967 2.1967ZM0.5 7C0.223858 7 0 7.22386 0 7.5C0 7.77614 0.223858 8 0.5 8H2.5C2.77614 8 3 7.77614 3 7.5C3 7.22386 2.77614 7 2.5 7H0.5ZM2.1967 12.8033C2.00144 12.608 2.00144 12.2915 2.1967 12.0962L3.61091 10.682C3.80617 10.4867 4.12276 10.4867 4.31802 10.682C4.51328 10.8772 4.51328 11.1938 4.31802 11.3891L2.90381 12.8033C2.70854 12.9986 2.39196 12.9986 2.1967 12.8033ZM12.5 7C12.2239 7 12 7.22386 12 7.5C12 7.77614 12.2239 8 12.5 8H14.5C14.7761 8 15 7.77614 15 7.5C15 7.22386 14.7761 7 14.5 7H12.5ZM10.682 4.31802C10.4867 4.12276 10.4867 3.80617 10.682 3.61091L12.0962 2.1967C12.2915 2.00144 12.608 2.00144 12.8033 2.1967C12.9986 2.39196 12.9986 2.70854 12.8033 2.90381L11.3891 4.31802C11.1938 4.51328 10.8772 4.51328 10.682 4.31802ZM8 12.5C8 12.2239 7.77614 12 7.5 12C7.22386 12 7 12.2239 7 12.5V14.5C7 14.7761 7.22386 15 7.5 15C7.77614 15 8 14.7761 8 14.5V12.5ZM10.682 10.682C10.8772 10.4867 11.1938 10.4867 11.3891 10.682L12.8033 12.0962C12.9986 12.2915 12.9986 12.608 12.8033 12.8033C12.608 12.9986 12.2915 12.9986 12.0962 12.8033L10.682 11.3891C10.4867 11.1938 10.4867 10.8772 10.682 10.682ZM5.5 7.5C5.5 6.39543 6.39543 5.5 7.5 5.5C8.60457 5.5 9.5 6.39543 9.5 7.5C9.5 8.60457 8.60457 9.5 7.5 9.5C6.39543 9.5 5.5 8.60457 5.5 7.5ZM7.5 4.5C5.84315 4.5 4.5 5.84315 4.5 7.5C4.5 9.15685 5.84315 10.5 7.5 10.5C9.15685 10.5 10.5 9.15685 10.5 7.5C10.5 5.84315 9.15685 4.5 7.5 4.5Z" fill="currentColor" fillRule="evenodd" clipRule="evenodd"  className="fill-accentTextContrast dark:fill-accentTextContrast-dark"></path></svg>
 
@@ -37,8 +64,8 @@ function Navbar(){
 
                             <span className="w-6 h-6 rounded-full bg-white dark:bg-gray-800 block float-right dark:float-left"></span>
                             {/* <span className="hidden dark:inline">🌛</span> */}
-         
-
+                            </motion.div>
+                            </AnimatePresence>
                         </label>
                     </div>
                     </div>
