e997110a2175b363a9eb3590cde866e8a6d2cbc6
Jaffar Hussein <jaffhussein@icloud.com>
2023-03-23 18:52:54 +0100

    Remove the text decoration on the logo on responsicess

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -279,7 +279,7 @@ function Navbar() {
 						<div className='fixed inset-0 z-50' />
 						<Dialog.Panel className='fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-accentBgSubtle dark:bg-accentBgSubtle-dark px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10'>
 							<div className='flex items-center justify-between'>
-								<a href='#' className='-m-1.5 p-1.5'>
+								<div className='-m-1.5 p-1.5'>
 									<span className='sr-only'>Your Company</span>
 									<img
 										className='block dark:hidden h-8 w-auto'
@@ -291,7 +291,7 @@ function Navbar() {
 										src={logo}
 										alt=''
 									/>
-								</a>
+								</div>
 								<button
 									type='button'
 									className='-m-2.5 rounded-md p-2.5 text-accentText dark:text-accentText-dark'
